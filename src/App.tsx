5eb43c1af15468710869b263c6adfc7952995cc4
sugama <reiji.sugama@gmail.com>
2024-07-15 20:55:09 +0900

    useStateの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 export const App = () => {
+  const [text, setText] = useState("初期値")
+
   return (
     <>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <input type='submit' value="追加" onSubmit={(e) => e.preventDefault()} />
       </form>
+      <p>{text}</p>
     </>
   );
 };
