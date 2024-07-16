c42839ebbf1543e2999bb5a8cf1ee9335437c572
sugama <reiji.sugama@gmail.com>
2024-07-16 11:11:34 +0900

    TODOの入力から一覧の取得までを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
 import { useState } from "react";
 
+type Todo = {
+  readonly id: number
+  value: string
+};
+
 export const App = () => {
   const [text, setText] = useState("初期値")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setText(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    const newTodo: Todo = {
+      id: new Date().getTime(),
+      value: text
+    }
+    // set関数には関数を渡すことができる。その関数は前回のstateを引数に受け取ることができる。
+    setTodos((todos) => [newTodo, ...todos])
+    setText('')
+  }
 
   return (
     <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-        <input type='submit' value="追加" onSubmit={(e) => e.preventDefault()} />
+      <form onSubmit={(e) => {
+        e.preventDefault(); 
+        handleSubmit();
+      }}>
+        <input type="text" value={text} onChange={(e) => handleText(e)} />
+        <input type='submit' value="追加" />
       </form>
-      <p>{text}</p>
+      <h2>記録されたタスク</h2>
+      <ul>
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.value}</li>
+      })}
+      </ul>
     </>
   );
 };
